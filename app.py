7b95f8ba3bba8e54eda8c15c29c25c95ff43040d
gowthamcastro007 <58845860+gowthamcastro007@users.noreply.github.com>
2023-12-21 13:01:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ ip_address = socket.gethostbyname(hostname)
 import os
 os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
 os.system("dpkg -x google-chrome-stable_current_amd64.deb $PWD")
+os.system("echo $PWD")
 os.system("export PATH='$PATH:$PWD'")
 
 
