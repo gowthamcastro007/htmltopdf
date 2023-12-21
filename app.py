e639f34a0f9bf5df26d961756b618faf015aec37
gowthamcastro007 <58845860+gowthamcastro007@users.noreply.github.com>
2023-12-21 13:07:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ ip_address = socket.gethostbyname(hostname)
 
 
 import os
-os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-os.system("dpkg -x google-chrome-stable_current_amd64.deb $PWD")
+# os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+# os.system("dpkg -x google-chrome-stable_current_amd64.deb $PWD")
 os.system("echo $PWD")
-os.system("export PATH='$PATH:$PWD'")
+# os.system("export PATH='$PATH:$PWD'")
 
 
 from selenium import webdriver
